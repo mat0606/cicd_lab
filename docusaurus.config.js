71bfa6a202a72b92c2bde7c45c092bc3542ac7b2
mat0606 <mat0606@hotmail.com>
2023-03-11 09:14:37 +0000

    fixed configuration file error

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'github.com',
+  url: 'https://github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/cicd_lab/',
