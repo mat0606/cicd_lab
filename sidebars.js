76771cd4b407f0ec5b41428a6957058897b6d785
mat0606 <mat0606@hotmail.com>
2023-03-14 03:34:07 +0000

    fixed missing picture and intro

--- sidebars.js
+++ sidebars.js
@@ -1,9 +1,10 @@
 module.exports = {
   tutorialSidebar:[
+    "intro",
     {
       type: 'category',
       label: 'Lab Overview',
-      items: ["intro/intro", "intro/Lab", "intro/Lab-Infra"]
+      items: ["intro/Lab", "intro/Lab-Infra"]
     },
     {
       type: 'category',
